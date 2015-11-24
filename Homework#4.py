7143335b6bc29a998ba74ce57366dd63e5845a0f
Askhat Sakebaev <sakebaev@gmail.com>
2015-11-24 07:30:29 +0600

    made some corrections in forth homework

--- Homework#4.py
+++ Homework#4.py
@@ -2,7 +2,7 @@ from Homework_4 import *
 
 inp = int(raw_input('insert integer from 0 to 9 '))
 if 0<=inp<=3:
-    print(firstfunc())
+    print(first.firstfunc())
 elif 4<=inp<=6:
     print(secondfunc())
 elif 7<=inp<=9:
