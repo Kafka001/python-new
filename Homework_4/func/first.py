ac57627b68e8865624fda096f7aea079303032a7
Askhat Sakebaev <sakebaev@gmail.com>
2015-11-30 00:56:34 +0600

    add new func

--- Homework_4/func/first.py
+++ Homework_4/func/first.py
@@ -2,3 +2,9 @@ def firstfunc():
     n = str(raw_input('insert data '))
     z = int(raw_input('how many times i need to show? '))
     return n*z
+
+def secondfunc():
+    rrr = inp
+    for i in range(1,11):
+        print (rrr)
+        rrr += 1
